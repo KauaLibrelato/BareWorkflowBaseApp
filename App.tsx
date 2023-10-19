923424d5c6bb442c25468798b6dfc33137d6ec1c
Kaua Librelato da Costa <kaualibrelatodacosta@gmail.com>
2023-10-19 17:31:43 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, View } from "react-native";
+
+export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+
+  return <View style={styles.container} />;
+}
